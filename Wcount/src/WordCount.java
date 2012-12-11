d239c8a594660f58adabf8b4a6ee5dc981a102e5
sunelsnn <sunelsnn@gmail.com>
2012-12-11 13:34:31 -0800

    executable new changes

--- Wcount/src/WordCount.java
+++ Wcount/src/WordCount.java
@@ -7,6 +7,7 @@
 	import org.apache.hadoop.conf.*;
 	import org.apache.hadoop.io.*;
 //	import org.apache.hadoop.mapred.OutputCollector;
+// import org.apache.hadoop.mapred.JobConf;
  import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -14,12 +15,30 @@ import org.apache.hadoop.util.*;
 	
 	public class WordCount {
 	
-	    public static class Map extends Mapper<Object, Text, Text, IntWritable> {
+	    public static class WCMap extends Mapper<Object, Text, Text, IntWritable> {
 	      private final static IntWritable one = new IntWritable(1);
 	      private Text word = new Text();
 	  //  private LongWritable word = new LongWritable() ;
-	
+	      
+	      protected void setup (Context context) throws IOException, InterruptedException
+	      {
+	    	  
+	    	  super.setup(context) ;
+	    	  System.out.println("In MAP setup ") ;
+	    	  Runtime rt = Runtime.getRuntime();
+	    	  String tempfilename =  "touch " + new  String ("~/file") + new  String (  Long.toString(new Date().getTime())) ;
+	    	  Process proc = rt.exec( "touch file1");
+	    	  Configuration conf = context.getConfiguration();
+	    	  System.out.println( conf.get("map.input.file")) ;
+	    	  System.out.println( conf.toString()) ;
+	    	 // System.out.println( conf.) ;
+	      }
 	      public void map(Object key, Text value, Context context ) throws IOException , InterruptedException {
+	    	  
+	    	  Configuration conf = context.getConfiguration();
+	    	  String param = conf.get("PARM1");
+	    	  System.out.println("pringint PARM" + param) ;
+	    	  
 	        String line = value.toString();
 	        StringTokenizer tokenizer = new StringTokenizer(line);
 	        while (tokenizer.hasMoreTokens()) {
@@ -29,25 +48,49 @@ import org.apache.hadoop.util.*;
 	      }
 	    }
 	
-	    public static class Reduce extends  Reducer<Text, IntWritable, Text, IntWritable> {
-	      public void reduce(Text key, Iterator<IntWritable> values, Context context) throws IOException , InterruptedException  {
+	    public static class WCReduce extends  Reducer<Text, IntWritable, Text, IntWritable> {
+	      public void reduce(Text key, Iterable<IntWritable> valuesobj, Context context) throws IOException , InterruptedException  {
+	    	  Configuration conf = context.getConfiguration();
+	    	  String param = conf.get("PARM1");
+	    	  System.out.println("pringint PARM in reducer" + param) ;
 	        int sum = 0;
+	        
+	        Iterator<IntWritable> values = valuesobj.iterator() ; 
 	        while (values.hasNext()) {
 	          sum += values.next().get();
 	        }
+	        
+	        /*
+	        for (IntWritable val: values)
+	        {
+	        	sum += val.get() ;
+	        }
+	        */
 	        context.write(key, new IntWritable(sum));
 	      }
 	    }
-	
+	    /*
+
+	    public void testfunction()
+	    {}
+	    */
 	    public static void main(String[] args) throws Exception {
+	    	System.out.println("Entering job config dsfds") ;
+	    	// System.in.read() ;
 	    	Configuration conf = new Configuration();
+	    	 conf.set("PARM1", "PARM1value");
+		      conf.set("PARM2", "PARM2value") ;
 	      Job job = new Job (conf, "WordCount");
 	      job.setJarByClass(WordCount.class);
-	      job.setMapperClass(Map.class);
-	      job.setCombinerClass(Reduce.class);
-	      job.setReducerClass(Reduce.class);
+	      job.setMapperClass(WCMap.class);
+	      job.setCombinerClass(WCReduce.class);
+	      job.setReducerClass(WCReduce.class);
 	      job.setOutputKeyClass(Text.class);
 	      job.setOutputValueClass(IntWritable.class);
+	     
+	      
+	 // job.setLong("temparm","someval") ;
+	   
 	      /*
 	      conf.setJar("Wcountjar.jar") ;
 	      conf.setJobName("WordCount");
@@ -77,7 +120,9 @@ import org.apache.hadoop.util.*;
 	      System.exit(job.waitForCompletion(true) ? 0 : 1);
 	
 	     // JobClient.runJob(conf);
-	    }
+	    
 	}
+	    
+	    }
 	
 
